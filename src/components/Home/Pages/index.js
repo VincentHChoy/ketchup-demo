1186266d678dea183fac9f8b5e22986101cb6268
donnahuang <donnaphzj@gmail.com>
2022-09-19 20:41:03 -0400

    mailjs

--- src/components/Home/Pages/index.js
+++ src/components/Home/Pages/index.js
@@ -5,11 +5,33 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Sidebar from "../../Sidebar/Sidebar";
 import Button from "../../Button/Button";
 
+const email_regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; 
+
+const EMAIL_JS_SERVICE_ID = 'service_z3ywa1s';
+const EMAIL_JS_TEMPLATE_ID = 'template_ep7mphh';
+const EMAIL_JS_PUBLIC_KEY = 'EKnx5SInPFQG3jWSG';
+
 const FALLBACK_PHOTO_URL = "https://4.bp.blogspot.com/-NiUcogaBYrk/UioQgTmkGuI/AAAAAAAAClg/YOdyn5RB4W4/s1600/minion_icon_image_picfishblogspotcom+%25287%2529.png"
 
 function Home() {
+  const [email, setEmail] = React.useState('');
 
   const { photoURL, displayName } = auth.currentUser;
+
+
+
+  const sendEmail = () => {
+    if (!email_regex.test(email)) {
+      alert('invalid email')
+    } else {
+      // eslint-disable-next-line no-undef
+      emailjs.send(EMAIL_JS_SERVICE_ID, EMAIL_JS_TEMPLATE_ID, {
+        link: 'localhost:3000',
+        user_email: email,
+      }, EMAIL_JS_PUBLIC_KEY);
+
+    }
+  }
 
   return (
   
@@ -28,15 +50,14 @@ function Home() {
           </span>
         </span>
 
-        <form className="left-20 text-2xl pb-5 bg-white" onSubmit={""}>
           <section className="flex flex-col content-center justify-center mt-24">
-            <input
+        <input
+          onChange={(e) => setEmail(e.target.value)}
               className="w-96 my-5 text-base text-primary outline-none border-b-2 border-rgb(83, 82, 82)"
               placeholder="type in your colleagues email to start collaborating"
             />
-            <Button message={"Send"} type="submit" />
+            <Button handleClick={sendEmail} message={"Send"} />
           </section>
-        </form>
       </main>
  
   );
