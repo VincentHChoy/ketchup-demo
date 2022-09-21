b095c5707bda7d9c3dec9b744f60d691c67505e3
donnahuang <donnaphzj@gmail.com>
2022-09-20 21:29:50 -0400

    add iframe for google doc and google sheet

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,17 +3,27 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ChatRoom from "../../components/ChatRoom/ChatRoom";
 import Sidebar from "../Sidebar/Sidebar";
 import HomePage from "./Pages/index";
+import './Pages/Home.css'
 
 function Home() {
   return (
-    <main className="flex flex-row justify-center item-center">
+    <main style={{width:'100%', height: '100%', overflow: 'visible'}}className="flex flex-row justify-center item-center">
       <Sidebar />
       <Routes>
         <Route path="/" element={<HomePage />}/>
         <Route path="chat" element={<ChatRoom />} />
         <Route path="/chat/:chatId" element={<ChatRoom />} />
-        <Route path="docs" element={<div>Docs Placeholder</div>} />
-        <Route path="sheets" element={<div>Sheets Placeholder</div>} />
+        <Route path="docs" element={
+
+          <iframe style={{ marginLeft: '80px', width: '100%', height: '100vh' }} class='googleweb' src="https://docs.google.com/document/d/1TbQXeYswsVU67zXm8MCco2a38bjo_o-1kC-al4kvuqg/edit" title="Google Docs"></iframe>
+
+        } />
+
+        <Route path="sheets" element={
+
+          <iframe style={{ marginLeft: '80px', width: '100%', height: '100vh' }} class='googleweb' src="https://docs.google.com/spreadsheets/d/1afkoYrMWjT6DzmVvslWtReYWEDRqw13H7LP-Sz8f09M/edit#gid=0" title="Google Sheets"></iframe>
+
+        } />
       </Routes>
     </main>
   );
